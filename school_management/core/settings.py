afa09ce157f86bd29887c24d1cead4b1c964b995
Abdun Nur Tomal <tomal.nur@gmail.com>
2024-12-05 00:19:03 +0600

    - model created

--- school_management/core/settings.py
+++ school_management/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'student_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'School_Info',
+        'USER': 'postgres',
+        'PASSWORD': 'Tomal.2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- school_management/student_app/models.py
+++ school_management/student_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Teachers(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=False)
+    email = models.CharField(max_length=150)
+    joining_date = models.DateField()
+    educational_background = models.JSONField(blank=True)
+    salary = models.FloatField(null=False,blank=False)
